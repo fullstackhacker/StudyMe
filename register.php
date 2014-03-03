04027f1a735d14b4a57a6cb4d9d936a9f6a10fd4
kapadiamush <kapadiamush@gmail.com>
2014-03-03 15:07:06 -0500

    added a working register php script... need to figure out security...

---
+++ register.php
@@ -0,0 +1,27 @@
+<?php
+//database info
+$server = "localhost"; 
+$username = "root";
+$password = "ToPsea906";
+$database = "studyme";
+$table = "Users";
+
+//connect to the database
+$con = mysqli_connect($server, $username, $password, $database); 
+
+//check connection
+if(mysqli_connect_errno()){
+	echo "Failed to connect to sql server<br>"; 
+}	
+
+$query = "INSERT INTO $table (firstname, lastname, email, password, usertype) VALUES ('$_POST[fname]','$_POST[lname]','$_POST[email]', 'dick' ,'$_POST[type]');";
+
+if(!mysqli_query($con, $query)){
+	die('ERROR ' . mysqli_error($con));
+}
+
+echo "<br>Registered Successfully!<br>";
+
+mysqli_close($con);
+
+?>
